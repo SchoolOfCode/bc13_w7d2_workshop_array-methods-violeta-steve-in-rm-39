05f54b3b9ac4b41254b7d5c8448324b00d2d80f4
violetacf <violeta@cique.es>
2022-11-08 12:29:52 +0000

    Task 3

--- task3.js
+++ task3.js
@@ -1,1 +1,11 @@
 let someNumbers = [4, 5, 6, 78, 2, 3, 45, 34, 2, 23, 5, 45, 6, 7, 23];
+
+const multipleOf3 = (number) => number % 3 === 0
+const multipleOf5 = (number) => number % 5 === 0
+const multipleOf60 = (number) => number % 60 === 0
+const multipleOf90 = (number) => number % 90 === 0
+
+console.log(someNumbers.some(multipleOf3))
+console.log(someNumbers.some(multipleOf5))
+console.log(someNumbers.some(multipleOf60))
+console.log(someNumbers.some(multipleOf90))
