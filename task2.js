8d2b6e67796c17391c8415ec62b75f8d35961877
Sunbearian <stigcollins@icloud.com>
2022-11-08 12:18:39 +0000

    task 2 complete

--- task2.js
+++ task2.js
@@ -1,11 +1,15 @@
 let animals = [
-    "baboon",
-    "kangaroo",
-    "rhino",
-    "frog",
-    "beaver",
-    "horse",
-    "basalisk",
-  ];
-  
-let bListAnimals = // Your 'filter' code here.
+	"baboon",
+	"kangaroo",
+	"rhino",
+	"frog",
+	"beaver",
+	"horse",
+	"basalisk",
+];
+
+let bListAnimals = animals.filter((animal) => {
+	return animal.charAt(0) === "b";
+});
+
+console.log(animals, bListAnimals);
